04eadf8484300fa3588685dfc7b805805633e12a
Leon <6860571@stud.nau.edu.ua>
2023-07-26 17:14:40 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles/App.css'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="container__logo"></div>
+      <div className="container__content">
+        <div className="container_card-list"></div>
+        <div className="container_card-info"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
